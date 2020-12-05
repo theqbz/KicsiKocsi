bd2b41fd4f1de1a785a7ada6acf04cd41d7148c6
QBZ <q.bz@hotmail.com>
2020-12-05 18:46:02 +0100

    Add project files.

---
+++ kicsikocsi/kicsikocsi.ino
@@ -0,0 +1,24 @@
+/*
+ Name:		kicsikocsi.ino
+ Created:	12/5/2020 6:14:38 PM
+ Author:	qbz
+*/
+
+//KICSIKOCSI TÁVIRÁNYÍTÓ
+
+
+#include <SPI.h>
+#include <RF24.h>
+#include <RF24_config.h>
+#include <printf.h>
+#include <nRF24L01.h>
+#include <MPU6050.h>
+
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
