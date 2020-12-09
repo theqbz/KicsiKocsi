c621ec258685d9a8b01e0fd345e13154ea2636e9
QBZ-laptop <q.bz@hotmail.com>
2020-12-09 20:51:38 +0100

    Lokátor létrehozása

---
+++ lokator/lokator.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		lokator.ino
+ Created:	12/9/2020 8:50:05 PM
+ Author:	qbz
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
