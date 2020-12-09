ccae96913e242a89557124f30745a54100c62e3a
QBZ <q.bz@hotmail.com>
2020-12-09 11:48:40 +0100

    Motor kész

--- kozpont/kozpont.ino
+++ kozpont/kozpont.ino
@@ -11,27 +11,31 @@ KICSIKOCSI KÖPONTI EGYSÉG
 #include <SPI.h>
 #include <RF24.h>
 #include <nRF24L01.h>
-#include "C:/Program Files (x86)/Arduino/hardware/arduino/avr/cores/arduino/Arduino.h"
-#include "C:/Program Files (x86)/Arduino/hardware/tools/avr/avr/include/stdlib.h"
+
+#define LIMIT 100				// a limit alatti értéknél nem indul a motor
+#define TIMEOUT 100				// riasztás, ha ennyi cikluson át nem üzen a távirányító
 
 // Arduino UNO Wifi R2 pin-kiosztás
 #define led 2
 #define MotE 3					// motor Enable
 #define MotC1 4					// motor táp1
 #define MotC2 5					// motor táp2
+#define Csipogo 6				// piezo hangszoro
 #define RfCS 8					// az nRF24L01 modul "Chip Set" lába
 #define RfCE 7					// az nRF24L01 modul "Chip Enable" lába
 #define SrvPin 10				// a servo motor vezérlõje
 
 // nRF24L01 rádió állandói
-RF24 radio(RfCE, RfCS);					// Rádió létrehozása
-const byte cim = 9654;					// a Rádió csatornájának címe
+RF24 radio(RfCE, RfCS);			// Rádió létrehozása
+const byte cim = 9654;			// a Rádió csatornájának címe
 
 // Servo
-Servo kormany;							// Szervó létrehozása
+Servo kormany;					// Szervó létrehozása
 
 // egyéb globális változók
-byte kuldemeny[3];						// a távirányítóból érkezõ adatok (elõre, oldalra, ok)
+byte kuldemeny[3];				// a távirányítóból érkezõ adatok (elõre, oldalra, ok)
+int kimaradas = 0;				// a távirányító elérhetetlenségének ideje
+bool NincsKapcsolat = true;		// a távírányító nincs csatlakozva
 
 void setup() {
 	pinMode(led, OUTPUT);
@@ -39,30 +43,38 @@ void setup() {
 	pinMode(MotC1, OUTPUT);
 	pinMode(MotC2, OUTPUT);
 	Serial.begin(9600);
-	radio.begin();						// Rádió bekapcsolása
-	radio.openReadingPipe(0, cim);		// csatorna megnyitása adatok fogadásához a távirányítótól
-	radio.setPALevel(RF24_PA_MIN);		// Rádió térerejének minimumra állítása
-	kormany.attach(SrvPin);				// Szervó vezérlõ csatlakoztatása
-	kormany.write(90);					// kormány kiegyenesítése
+	radio.begin();					// Rádió bekapcsolása
+	radio.openReadingPipe(0, cim);	// csatorna nyitása adatok fogadásához a távirányítótól
+	radio.setPALevel(RF24_PA_MIN);	// Rádió térerejének minimumra állítása
+	kormany.attach(SrvPin);			// Szervó vezérlõ csatlakoztatása
+	kormany.write(90);				// kormány kiegyenesítése
 	delay(2000);
 }
 
 void loop() {
-	radio.startListening();								// vevõ-módba kapcsolja a rádiót
+	radio.startListening();				// vevõ-módba kapcsolja a rádiót
 
-	if (radio.available())								// ha van fogadott adat (amit a távirányító küldött)
+	if (radio.available())				// ha van fogadott adat (amit a távirányító küldött)
 	{
+		if (NincsKapcsolat)
+		{
+			tone(Csipogo, 880, 60);
+			delay(250);
+			tone(Csipogo, 1760, 60);
+			NincsKapcsolat = false;
+		}
 		radio.read(&kuldemeny, sizeof(kuldemeny));				// adatok beolvasása
+		kimaradas = 0;
 		if (kuldemeny[2] == 1)
 		{
 			int sebesseg = map(kuldemeny[0], 0, 255, -255, 255);
-			if (sebesseg < -15)
+			if (sebesseg < -LIMIT)
 			{
 				digitalWrite(MotC1, LOW);
 				digitalWrite(MotC2, HIGH);
 				sebesseg = -sebesseg;
 			}
-			else if (sebesseg > 15)
+			else if (sebesseg > LIMIT)
 			{
 				digitalWrite(MotC1, HIGH);
 				digitalWrite(MotC2, LOW);
@@ -71,6 +83,7 @@ void loop() {
 			{
 				sebesseg = 0;
 			}
+
 			KuldemenyKiiras(sebesseg);
 
 			digitalWrite(led, HIGH);
@@ -85,11 +98,23 @@ void loop() {
 	else
 	{
 		Serial.println("valami nem oke");
+		digitalWrite(led, LOW);
+		kimaradas++;
+		NincsKapcsolat = true;
+	}
+	if (kimaradas == TIMEOUT)
+	{
+		tone(Csipogo, 1000, 40);
+		delay(100);
+		tone(Csipogo, 880, 20);
+		delay(100);
+		tone(Csipogo, 880, 20);
+		kimaradas = 0;
 	}
 	delay(20);
 }
 
-// debug függvények
+// debug
 
 void KuldemenyKiiras(int sebesseg) {
 	for (int i = 0; i < 3; i++)
