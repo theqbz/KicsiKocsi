a4ceeda204dc9806d914cc19a519cf0a2a7e6a0b
QBZ-laptop <q.bz@hotmail.com>
2020-12-09 21:07:19 +0100

    looocc

---
+++ locc/locc.ino
@@ -0,0 +1,32 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       locc.ino
+    Created:	2020. 12. 09. 21:01:28
+    Author:     QBZ-LAPTOP\qbz
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
