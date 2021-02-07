02883f3d4877a45d5aed1615722defa11c645bf7
QBZ-laptop <q.bz@hotmail.com>
2021-02-07 08:59:55 +0100

    stilisztikai javítások
    A kód nem változott csak a kommentek lettek átírva, pontosítva stb.

--- KpMotor.ino
+++ KpMotor.ino
@@ -1,6 +1,24 @@
 /*
-  Name:		MOTOR (KICSIKOCSI KOZPONTI EGYSEG)
-		Arduino Nano
+		MOTOR (KICSIKOCSI KOZPONTI EGYSEG)
+
+		kesztitette: Borsodi Zoltan
+			     github.com/theqbz
+
+	Arduino Nano:
+
+	A4  -  SDA (I2C kommunikacio)
+	A5  -  SCL (I2C kommunikacio)
+	D3  -  L293 'Input1' (elore forog a motor)
+	D4  -  L293 'Input2' (hatra forog a motor)
+	D5  -  A szervo motor vezerloje
+	D6  -  L293 'Enable' (motor bekapcsolasa)
+	D7  -  nRF24L01 'Chip Enable'
+	D8  -  nRF24L01 'Chip Set'
+	D9  -  piezzo hangkerlto /csipogo/
+	D11 -  nRF24L01 MOSI (Az ICSP kommunikaciohoz szukseges)
+	D12 -  nRF24L01 MISO (Az ICSP kommunikaciohoz szukseges)
+	D13 -  nRF24L01 SCK (Az ICSP kommunikaciohoz szukseges)
+
 
 */
 
@@ -12,35 +30,35 @@
 #include <Wire.h>
 #include <Servo.h>
 
-
 // Arduino Nano pin kiosztas
-#define MOTOR_ELORE 3								// L293 "Input 1" (elore forog a motor)
-#define MOTOR_HATRA 4								// L293 "Input 2" (hatra forog a motor)
-#define SERVO 5											// A szervo motor vezerloje
-#define MOTOR_ENABLE 6							// L293 "Enable" (motor bekapcsolasa)
-#define RF_CE 7											// nRF24L01 "Chip Enable"
-#define RF_CS 8											// nRF24L01 "Chip Set"
-#define PIEZZO 9										// csipogo
-#define RF_MOSI 11									// nRF24L01 MOSI (Az ICSP kommunikaciohoz szukseges)
-#define RF_MISO 12									// nRF24L01 MISO (Az ICSP kommunikaciohoz szukseges)
-#define RF_SCK 13										// nRF24L01 SCK (Az ICSP kommunikaciohoz szukseges)
+#define MOTOR_ELORE 3							
+#define MOTOR_HATRA 4							
+#define SERVO 5								
+#define MOTOR_ENABLE 6							
+#define RF_CE 7								
+#define RF_CS 8								
+#define PIEZZO 9							
+#define RF_MOSI 11							
+#define RF_MISO 12							
+#define RF_SCK 13							
 
 // egyeb allandok
-#define TIMEOUT 100									// riasztas, ha ennyi cikluson át nem uzen a taviranyito
-#define AKADALY 20									// ennel kozelebb (centimeterben)
+#define TIMEOUT 100							// ennyi ciklus utan riaszt, ha offline a taviranyito
+#define AKADALY 20							// ennel kozelebb (centimeterben)
 #define MIN_SEBESSEG 100						// limit alatti erteknel nem indul a motor
 
 
 RF24 radio(RF_CE, RF_CS);						// Radio letrehozasa
-Servo kormany;											// Kormany letrehozasa
+Servo kormany;								// Kormany letrehozasa
 
 byte kontrollerSebesseg;						// A taviranyito altal kuldott sebesseg adat
 byte kontrollerKormany;							// A taviranyito altal kuldott kormanyzas adat
-bool kontrollerGomb;								// A taviranyito gombjanak allapota
-int kontrollerOfflineTimer;					// A taviranyito offline allapotanak kesleltetese
+bool kontrollerGomb;							// A taviranyito gombjanak allapota
+int kontrollerOfflineTimer;						// A taviranyito offline allapotanak kesleltetese
 bool kontrollerOnline;							// A taviranyito online allapota
-byte elsoLokator;										// Az elso lokator altal mert tavolsag
-byte hatsoLokator;									// A hatso lokator altal mert tavolsag
+byte elsoLokator;							// Az elso lokator altal mert tavolsag
+byte hatsoLokator;							// A hatso lokator altal mert tavolsag
+bool akadalyok[2];							// Marker az akadaly jelzesehez
 
 void mozgas()
 {
@@ -97,22 +115,58 @@ void lettKapcsolat()
 	kontrollerOnline = true;
 }
 
+void akadalyJelzes(byte irany)
+{
+	if (!akadalyok[irany]) {
+		tone(PIEZZO, 440, 20);
+		delay(100);
+		tone(PIEZZO, 440, 20);
+		akadalyok[irany]=true;
+	}
+}
+
 int sebessegSzamitasa()
 {
 	if (kontrollerGomb == 1 && kontrollerOnline) {
 		int sebesseg = map(kontrollerSebesseg, 0, 255, -255, 255);
-		if (sebesseg < -MIN_SEBESSEG) {														// ha a sebesseg alapjan hatrament van es nagyobb a minimumnal
+
+		// ha a sebesseg alapjan hatrament van es a sebesseg (abszoluterteke) nagyobb a minimumnal
+		if (sebesseg < -MIN_SEBESSEG) {
 			digitalWrite(MOTOR_ELORE, LOW);
 			digitalWrite(MOTOR_HATRA, HIGH);
-			if (hatsoLokator < AKADALY) sebesseg = 0;								// ha a hatso lokotor alapjan kozel egy akadaly
-			else sebesseg = -sebesseg;															// a negativ sebessegertek abszoluterteke
+
+			// ha a hatso lokotor alapjan kozel egy akadaly
+			if (hatsoLokator < AKADALY) {
+				sebesseg = 0;
+				akadalyJelzes(0);
+			}
+
+			// ha nincs akadaly, akkor a negativ sebessegertek abszoluterteke
+			else {
+				sebesseg = -sebesseg;
+				akadalyok[0]=false;
+			}
 		}
-		else if (sebesseg > MIN_SEBESSEG) {												// ha eloremenet van es a sebesseg meghaladja a minimumiot
+
+		// ha eloremenet van es a sebesseg meghaladja a minimumiot
+		else if (sebesseg > MIN_SEBESSEG) {
 			digitalWrite(MOTOR_ELORE, HIGH);
 			digitalWrite(MOTOR_HATRA, LOW);
-			if (elsoLokator < AKADALY) sebesseg = 0;								// ha az elso lokotor alapjan kozel egy akadaly
+
+			// ha az elso lokotor alapjan kozel egy akadaly
+			if (elsoLokator < AKADALY) {
+				sebesseg = 0;
+				akadalyJelzes(1);
+			}
+
+		// ha nincs akadaly, akkor nincs kulon teendo a sebesseggel
+		akadalyok[1]=false;
 		}
-		else sebesseg = 0;
+
+		// amennyiben a sebesseg nem haladja meg a minimalis merteket, nincs mozgas
+		else {
+			sebesseg = 0;
+		}
 		return sebesseg;
 	}
 	else return 0;
@@ -144,11 +198,16 @@ void setup()
 	pinMode(PIEZZO, OUTPUT);
 	Serial.begin(9600);
 	Wire.begin();
-	radio.begin();                          // Radio bekapcsolasa
-	radio.openReadingPipe(0, 96);						// csatorna nyitasa adatok fogadasahoz a taviranyitotol
-	radio.setPALevel(RF24_PA_MIN);					// Radio tererejenek minimumra allitasa
-	kormany.attach(SERVO);									// Szervo csatlakoztatasa
-	kormany.write(90);											// kormany kiegyenesitese
+	// Radio bekapcsolasa
+	radio.begin();
+	// csatorna nyitasa adatok fogadasahoz a taviranyitotol
+	radio.openReadingPipe(0, 96);
+	// Radio tererejenek minimumra allitasa
+	radio.setPALevel(RF24_PA_MIN);
+	// Szervo csatlakoztatasa
+	kormany.attach(SERVO);
+	// kormany kiegyenesitese
+	kormany.write(90);
 }
 
 void loop()
